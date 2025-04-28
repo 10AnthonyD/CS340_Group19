d53bc594e4acd1cd01c98927c1c63705dff9409b
shkodykoshka <71207101+shkodykoshka@users.noreply.github.com>
2025-04-28 16:47:56 -0700

    Fixed error in Customer table

--- DDL.sql
+++ DDL.sql
@@ -10,7 +10,7 @@ CREATE TABLE Customers (
 	customer_id int(11) NOT NULL AUTO_INCREMENT,
 	first_name VARCHAR(255) NOT NULL,
 	last_name VARCHAR(255) NOT NULL,
-	email VARCHAR(255), NOT NULL
+	email VARCHAR(255) NOT NULL,
 	phone VARCHAR(15),
 	PRIMARY KEY (customer_id),
 	UNIQUE(email)
